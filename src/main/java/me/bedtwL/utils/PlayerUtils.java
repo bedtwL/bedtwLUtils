2ac0595d6ba71dca9713251c6446f00fad187d42
bedtwL <blueaustin0507@gmail.com>
2025-06-22 00:38:32 +0800

    PlayerUtils sendTitle for 1.8

---
+++ src/main/java/me/bedtwL/utils/PlayerUtils.java
@@ -0,0 +1,27 @@
+package me.bedtwL.utils;
+
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class PlayerUtils {
+    public void sendTitle(Player p, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+        if (title != null) {
+            if (!title.isEmpty()) {
+                IChatBaseComponent bc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}");
+                PacketPlayOutTitle tit = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, bc);
+                PacketPlayOutTitle length = new PacketPlayOutTitle(fadeIn, stay, fadeOut);
+                ((CraftPlayer) p).getHandle().playerConnection.sendPacket(tit);
+                ((CraftPlayer) p).getHandle().playerConnection.sendPacket(length);
+            }
+        }
+        if (subtitle != null) {
+            IChatBaseComponent bc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + subtitle + "\"}");
+            PacketPlayOutTitle tit = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, bc);
+            PacketPlayOutTitle length = new PacketPlayOutTitle(fadeIn, stay, fadeOut);
+            ((CraftPlayer) p).getHandle().playerConnection.sendPacket(tit);
+            ((CraftPlayer) p).getHandle().playerConnection.sendPacket(length);
+        }
+    }
+}
